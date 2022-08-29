db8e5136a97954141c1460f5017c516bab395af2
Adam Heidmann <adamdhm@yahoo.com>
2022-08-28 18:28:28 -0700

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Adam Heidmann on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
